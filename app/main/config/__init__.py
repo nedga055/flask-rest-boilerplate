32afeafa5a4df72b6964fe5199b913d4dc423546
Nick Edgar <edgar@76design.com>
2020-03-16 14:54:16 -0400

    Config elements are all strings. For those elements that should be an integer, convert them.

--- app/main/config/__init__.py
+++ app/main/config/__init__.py
@@ -10,13 +10,13 @@ class FlaskConfig:
     SECRET_KEY = config.get('flask', 'secret_key')
     DEBUG = config.get('flask', 'debug')
     JWT_SECRET_KEY = config.get('jwt', 'secret_key')
-    JWT_ACCESS_TOKEN_EXPIRES = config.get('jwt', 'access_token_expires')
+    JWT_ACCESS_TOKEN_EXPIRES = int(config.get('jwt', 'access_token_expires'))
 
 
 class DatabaseConfig:
     DB_TYPE = config.get('database', 'type')
     DB_HOST = config.get('database', 'host')
-    DB_PORT = config.get('database', 'port')
+    DB_PORT = int(config.get('database', 'port'))
     DB_NAME = config.get('database', 'name')
     DB_USER = config.get('database', 'user')
     DB_PASS = config.get('database', 'pass')
