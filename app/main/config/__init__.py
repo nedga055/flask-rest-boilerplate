a39491b636e47990a3fe00fe7a6f3907c64ef0f3
Nick Edgar <edgar@76design.com>
2020-03-16 13:53:51 -0400

    Set up database config object.

--- app/main/config/__init__.py
+++ app/main/config/__init__.py
@@ -11,3 +11,12 @@ class FlaskConfig:
     DEBUG = config.get('flask', 'debug')
     JWT_SECRET_KEY = config.get('jwt', 'secret_key')
     JWT_ACCESS_TOKEN_EXPIRES = config.get('jwt', 'access_token_expires')
+
+
+class DatabaseConfig:
+    DB_TYPE = config.get('database', 'type')
+    DB_HOST = config.get('database', 'host')
+    DB_PORT = config.get('database', 'port')
+    DB_NAME = config.get('database', 'name')
+    DB_USER = config.get('database', 'user')
+    DB_PASS = config.get('database', 'pass')
