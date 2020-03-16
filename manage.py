8863cf16f951e8b6e5cb58ce0f2d5277e6542c0e
Nick Edgar <edgar@76design.com>
2020-03-16 13:55:02 -0400

    Register the api blueprint and push the context onto the app.

--- manage.py
+++ manage.py
@@ -1,9 +1,13 @@
 from flask_script import Manager
 
+from app import blueprint
 from app.main import create_app
 
 # Create the app
 app = create_app()
+app.register_blueprint(blueprint)
+
+app.app_context().push()
 
 # Set up the manager
 manager = Manager(app)
