d976946e8ec995d634ebb26509b5c11db7acdf1a
Nick Edgar <edgar@76design.com>
2020-03-16 13:54:42 -0400

    Connect the database and set up JWT authentication

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from mongoengine import connect
+from flask_jwt_extended import JWTManager
 
-from .config import FlaskConfig
+from .config import FlaskConfig, DatabaseConfig
 
 
 def create_app():
     # Initialize flask app
     app = Flask(__name__)
     app.config.from_object(FlaskConfig)
-    # TODO: Initialize database
+    # Initialize database
+    # TODO: Would be nice to support other Database types
+    connect(
+        db=DatabaseConfig.DB_NAME,
+        host=DatabaseConfig.DB_HOST,
+        port=DatabaseConfig.DB_PORT,
+        username=DatabaseConfig.DB_USER,
+        password=DatabaseConfig.DB_PASS
+    )
+    # Initialize JWT authorization
+    JWTManager(app)
     return app
