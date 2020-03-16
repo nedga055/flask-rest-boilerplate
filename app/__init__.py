a01e97455728b2ad8dd57625d7d0763a9c217dde
Nick Edgar <edgar@76design.com>
2020-03-16 13:54:07 -0400

    Add authorization definition to the api.

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,15 @@ blueprint = Blueprint('api/v1', __name__)
 api = Api(blueprint,
           title="App Rest API V1",
           version="1.0",
-          description="Boilerplate Flask Rest API for CDO Data Science Team projects"
-          )
+          description="Boilerplate Flask Rest API for CDO Data Science Team projects",
+          authorizations={
+              'accessToken': {
+                  'type': 'apiKey',
+                  'in': 'header',
+                  'name': 'Authorization',
+                  'tokenUrl': '/access_token',
+                  'description': 'Use the access token after the keyword <b>Bearer</b>'
+              }
+          })
 
 # TODO: Add namespaces
