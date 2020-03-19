c23cf62085e7c268b9cac1a4fe6ad2a2aee65e64
Nick Edgar <edgar@76design.com>
2020-03-19 08:56:14 -0400

    Bash script for building app and managing conda environment.

---
+++ app.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+PREFIX=./api_env
+
+usage() {
+  echo -n "./app.sh [OPTION]...
+
+ Options:
+  -i, --init        Initialize the application
+  -u, --update      Update the conda environment
+  -e, --export      Export the conda environment
+  -h, --help        Display this help and exit
+"
+}
+
+if [ $# -gt 0 ]; then
+  while [ "$1" != "" ]; do
+    case $1 in
+      -i | --init )
+        echo "Initializing application"
+        # Create local config file if it doesn't already exist
+        EXAMPLE_FILE=./app/main/config/example.local.cfg
+        FILE=./app/main/config/local.cfg
+        if [ -f "$EXAMPLE_FILE" ] && [ ! -f "$FILE" ]; then
+          echo "Creating local configuration file."
+          cp "$EXAMPLE_FILE" "$FILE"
+        fi
+
+        # Create conda environment
+        if [ ! -d "$PREFIX" ]; then
+          echo "Creating conda environment"
+          conda env create -f environment.yml --prefix "$PREFIX"
+        fi
+        ;;
+      -u | --update )
+        echo "Updating conda environment"
+        conda env update -p "$PREFIX" --file environment.yml
+        ;;
+      -e | --export )
+        echo "Exporting conda environment"
+        conda env export > environment.yml
+        ;;
+      -h | --help )
+        usage
+        ;;
+    esac
+    shift
+  done
+else
+  echo "No arguments were provided.
+  "
+  usage
+fi
