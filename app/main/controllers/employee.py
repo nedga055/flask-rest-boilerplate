91e6ace0361807651e1b18d36acd3d5ae302992c
Collin Brown <collinbrown95@gmail.com>
2020-04-27 09:39:52 -0400

    small modification swagger doc string

--- app/main/controllers/employee.py
+++ app/main/controllers/employee.py
@@ -16,15 +16,15 @@ _new_employee = EmployeeDto.new_employee
 
 @api.route("",
            doc={
-               "description": "A resource to get all employees or to post an employee.",
+               "description": "Multiple employees.<br>",
            })
 class Employees(Resource):
-    @api.doc('List of employees')
+    @api.doc("List of employees", description="Gets a list of all employees currently stored in the database.")
     @api.marshal_list_with(_employee)
     def get(self):
         return list(get_all_employees())
 
-    @api.doc('Add employee')
+    @api.doc("Add employee", description="Adds an employee(s) to the database.")
     @api.expect(_new_employee, validate=True)
     @api.marshal_with(_employee, code=201)
     def post(self):
@@ -39,11 +39,11 @@ class Employees(Resource):
 
 @api.route("/<employee_id>",
            doc={
-               "description": "Retreives an employee, given the employee's ID"
+               "description": "Single employee.<br>"
            })
 @api.response(401, "Employee not found.")
 class Employee(Resource):
-    @api.doc(params={"employee_id": "The integer id assigned to the employee in the database."})
+    @api.doc("Single employee", description="Gets a single employee by their database id (for illustrative purposes, an integer starting at 0 that increments by 1).", params={"employee_id": "The integer id assigned to the employee in the database."})
     @api.marshal_with(_employee, 200)
     def get(self, employee_id):
         if get_employee_by_id(employee_id):
